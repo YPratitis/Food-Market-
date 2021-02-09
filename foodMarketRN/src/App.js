fafb11636750f9e958f7d65686e89ad3b3ed44f1
yohanes123 <yohanespratitis@gmail.com>
2021-02-09 22:40:49 +0700

    instalation and setup react navigation v.5

--- foodMarketRN/src/App.js
+++ foodMarketRN/src/App.js
@@ -1,12 +1,18 @@
+import 'react-native-gesture-handler';
 import React from 'react';
 import {
   SafeAreaView,
   ScrollView, StyleSheet, View
 } from 'react-native';
 import {SplashScreen} from './pages';
+import { NavigationContainer } from '@react-navigation/native';
 
 const App = ()  => {
-  return <SplashScreen />;
+  return (
+    <NavigationContainer>
+      <SplashScreen />
+    </NavigationContainer>
+  )
 };
 
 const styles = StyleSheet.create({
