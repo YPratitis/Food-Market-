10ee5d2da10fd38c2b7cff066a3b429309e5edb8
yohanes123 <yohanespratitis@gmail.com>
2021-02-09 20:44:59 +0700

    add react native svg, splashscreen page

---
+++ foodMarketRN/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import {
+  SafeAreaView,
+  ScrollView, StyleSheet, View
+} from 'react-native';
+import {SplashScreen} from './pages';
+
+const App = ()  => {
+  return <SplashScreen />;
+};
+
+const styles = StyleSheet.create({
+  
+});
+
+export default App;
---
+++ foodMarketRN/src/pages/SplashScreen/index.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import {Logo} from '../../assets';
+
+const SplashScreen = () => {
+    return (
+        <View 
+            style={{
+                backgroundColor: '#FFC700', 
+                flex:1,
+                justifyContent:'center',
+                alignItems:'center'}}>
+            <Logo />
+            <View style={{height:38}}/>
+            <Text style={{fontSize: 32, color: '#020202'}}>FoodMarket</Text>
+        </View>
+    )
+}
+
+export default SplashScreen;
+
+const styles = StyleSheet.create({})
